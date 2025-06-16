0b2b6c2b1ca689e0f701ade9f9568d3f2b5d38bd
Gowtham A <143709432+Gowtham-A-GM@users.noreply.github.com>
2025-06-16 23:23:29 +0530

    Update Constants.kt - Removed APIKey

--- app/src/main/java/com/example/gichat/viewModels/Constants.kt
+++ app/src/main/java/com/example/gichat/viewModels/Constants.kt
@@ -2,6 +2,4 @@ package com.example.gichat.viewModels
 
 object Constants {
 
-//    const val APIKEY = "AIzaSyAvGgefaf8qo59mK_s7cDKvobS_1cSpKgY"
-    const val APIKEY = "AIzaSyDW3-a1ErW0Qv2E39F51WVz9SpME7RQdFI"
 }
